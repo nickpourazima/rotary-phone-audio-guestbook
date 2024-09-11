d047ad0a1bb79131e782e582a2c2e476778afdb4
Mevel <4347330+Mevel@users.noreply.github.com>
2024-09-11 11:47:07 +0200

    add exceed und beep upload

--- webserver/server.py
+++ webserver/server.py
@@ -53,7 +53,7 @@ def download_file(filename):
 @app.route('/config', methods=['GET', 'POST'])
 def edit_config():
     if request.method == 'POST':
-        # Handle file upload
+        # Handle Greeting file upload
         if 'greeting_file' in request.files:
             greeting_file = request.files['greeting_file']
             if greeting_file.filename != '':
@@ -62,6 +62,26 @@ def edit_config():
                 greeting_file.save(greeting_path)
                 # Update the greeting path in the config
                 config['greeting'] = normalize_path(greeting_path.relative_to(Path(__file__).parent.parent))
+
+        # Handle Beep file upload
+        if 'beep_file' in request.files:
+            beep_file = request.files['beep_file']
+            if beep_file.filename != '':
+                beep_filename = beep_file.filename
+                beep_path = upload_folder / beep_filename
+                beep_file.save(beep_path)
+                # Update the beep path in the config
+                config['beep'] = normalize_path(beep_path.relative_to(Path(__file__).parent.parent))
+
+        # Handle Time Exceeded file upload
+        if 'time_exceeded_file' in request.files:
+            time_exceeded_file = request.files['time_exceeded_file']
+            if time_exceeded_file.filename != '':
+                time_exceeded_filename = time_exceeded_file.filename
+                time_exceeded_path = upload_folder / time_exceeded_filename
+                time_exceeded_file.save(time_exceeded_path)
+                # Update the time exceeded path in the config
+                config['time_exceeded'] = normalize_path(time_exceeded_path.relative_to(Path(__file__).parent.parent))
 
         # Update the config fields without losing comments
         config['alsa_hw_mapping'] = request.form['alsa_hw_mapping']
@@ -77,14 +97,12 @@ def edit_config():
         config['record_greeting_gpio'] = int(request.form['record_greeting_gpio'])
         config['record_greeting_type'] = request.form['record_greeting_type']
         config['record_greeting_bounce_time'] = float(request.form['record_greeting_bounce_time'])
-        config['beep'] = normalize_path(request.form['beep'])
         config['beep_volume'] = float(request.form['beep_volume'])
         config['beep_start_delay'] = float(request.form['beep_start_delay'])
         config['beep_include_in_message'] = request.form['beep_include_in_message'] == 'True'
         config['greeting'] = config.get('greeting', request.form['greeting'])
         config['greeting_volume'] = float(request.form['greeting_volume'])
         config['greeting_start_delay'] = float(request.form['greeting_start_delay'])
-        config['time_exceeded'] = normalize_path(request.form['time_exceeded'])
         config['time_exceeded_volume'] = float(request.form['time_exceeded_volume'])
         config['recordings_path'] = normalize_path(request.form['recordings_path'])
         config['time_exceeded_length'] = int(request.form['time_exceeded_length'])
